52f5d6dc88648ec8a6aaaf2db16963d9d4798ab3
Siva <siva@user.com>
2023-03-11 22:31:44 -0600

    DatabaseProject upload

---
+++ src/test/DeleteExample.java
@@ -0,0 +1,32 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteExample {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "Learn#3123");
+		Statement stmt = con.createStatement();
+
+		stmt.executeUpdate("delete from eproduct where name='Television'");
+
+		ResultSet result = stmt.executeQuery("select * from eproduct");
+
+		while (result.next()) {
+
+			System.out.println("Product Id " + result.getInt("ID"));
+			System.out.println("Product name " + result.getString("name"));
+			System.out.println("Product price is " + result.getDouble("price"));
+		}
+
+		con.close();
+
+	}
+
+}
